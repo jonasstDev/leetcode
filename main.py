50d58adfef02fde0ccaf8de3942f2d9d3790847f
jonasstDev <jonas.strehler@t-online.de>
2024-01-30 11:36:52 +0700

    test if see in readme

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def is_palindrome(candidate):
 
 def fibonacci(n):
     if n <= 0:
-        return "Invalid input. Please enter a positive integer."
+        return "ERROR"
     elif n == 1:
         return 0
     elif n == 2:
